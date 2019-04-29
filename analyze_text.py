3017c6524955c4fd321bb4ad640484f7f3ef0326
csmith158 <49175437+csmith158@users.noreply.github.com>
2019-04-28 22:41:59 -0600

    Update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -1,4 +1,20 @@
-# Remember to reach out for help after your own due diligence
+un# Remember to reach out for help after your own due diligence
 
 def analyze_text(text):
-    # Your code here
+    e_count = 0
+    for i in (text):
+        if i == "e":
+            e_count = e_count + 1
+        elif i == "E":
+            e_count = e_count +1
+    
+    total_count = 0
+    
+    for i in (text):
+        if i.isalpha():
+            total_count = total_count + 1
+        elif i.islower():
+            total_count = total_count + 1
+    
+    answer = "The text contains {0} alphabetic characters, of which {1} ({2}%) are ‘e’"
+    print(answer.format(total_count, e_count, ((e_count/total_count)*100):.2f))
